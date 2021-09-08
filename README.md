7ee0d4b2ea8c24f1ccb6347d55113e89b606bd80
Jason Goossens <jason.goossens@gmail.com>
2021-09-08 10:30:28 +0200

    init

--- README.md
+++ README.md
@@ -1,19 +1,34 @@
 # portfolio_v2
 
+## Description
+
+### Structure
+
+-   home/landing page (short animation?)
+-   about me
+-   skills
+-   projects
+-   CV
+-   contact
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div
+        v-for="(project, index) in projects.items"
+        :key="index"
+        class="project"
+    >
+        <h1>{{ project.name }}</h1>
+        <ul>
+            <li
+                v-for="(technology, index) in project.technologies"
+                :key="index"
+            >
+                {{ technology.name }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import projects from './data/projects';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: {
+        HelloWorld,
+    },
+    data() {
+        return { projects };
+    },
+};
+console.log(projects);
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
